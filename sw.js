9158453cb5d6d56b0b2476f031899bc5c50b8293
dsheller85 <dale.sheller@gmail.com>
2026-08-02 08:54:22 -0500

    Add IronMinute PWA (app, service worker, manifest, icon)

---
+++ sw.js
@@ -0,0 +1,20 @@
+/* IronMinute offline cache */
+const C = "ironminute-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(FILES).catch(() => {})));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== C).map(x => caches.delete(x)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(C).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
